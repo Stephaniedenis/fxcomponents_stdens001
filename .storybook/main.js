1fdfd6d180475edcde3947f7d93cff12b9dc898f
Stephaniedenis <93406932+Stephaniedenis@users.noreply.github.com>
2022-11-03 09:37:13 -0400

    [PATCH] Modification du main

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,6 @@
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  framework: "@storybook/react",
-  staticDirs: [{ from: '../src/assets', to: '/assets' }]
+	stories: ["../**/*.stories.mdx", "../**/*.stories.@(js|jsx|ts|tsx)"],
+	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+	framework: "@storybook/react",
+	staticDirs: [{ from: "../assets", to: "/assets" }],
 };
